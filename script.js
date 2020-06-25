43404fd2a18a9f4d63e09f091bb566967e4395d7
kayhill <61764488+kayhill@users.noreply.github.com>
2020-06-25 15:16:30 -0400

    nav and side nav, collapsible

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-function navdrop() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+document.addEventListener('DOMContentLoaded', function() {
+  var elem = document.querySelector('.sidenav');
+  var instance = new M.Sidenav(elem);
   
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, options);
+  
+
+});
+
+
+
