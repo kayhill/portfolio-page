fd68d956074095f69db99e49d610eee6c9be02c9
kayhill <61764488+kayhill@users.noreply.github.com>
2020-06-24 21:20:58 -0400

    added material design style sheet

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function navdrop() {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
+      
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
         if (openDropdown.classList.contains('show')) {
